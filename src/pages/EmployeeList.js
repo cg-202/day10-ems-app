136715c4d59c010ca52f008305f8b946cb6166db
santosh <santosh.ece06@gmail.com>
2021-07-03 17:34:23 +0530

    fetching the list from the server.

--- src/pages/EmployeeList.js
+++ src/pages/EmployeeList.js
@@ -1,7 +1,41 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllEmployeeAction } from "../redux/store";
+
 export const EmployeeList = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  useEffect(() => dispatch(getAllEmployeeAction()), []);
+
   return (
     <div>
-      <h1>Employee List</h1>
+      <div className="alert alert-secondary">
+        <h3>Employee List</h3>
+      </div>
+
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Username</th>
+            <th scope="col">Password</th>
+            <th scope="col">Email</th>
+            <th scope="col">Mobile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.employeeList.map((item, index) => (
+            <tr key={index}>
+              <th scope="row">{item.id}</th>
+              <td>{item.userName}</td>
+              <td>{"*****"}</td>
+              <td>{item.email}</td>
+              <td>{item.mobile}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,40 @@
 import { applyMiddleware, createStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
+import axios from "axios";
 
 const initState = {
   employeeList: [],
 };
 
+// ACTION TYPES
+const EMPLOYEE_GET_ALL_ACTION_TYPE = "EMPLOYEE_GET_ALL_ACTION_TYPE";
+const EMPLOYEE_GET_BY_ID_ACTION_TYPE = "EMPLOYEE_GET_BY_ID_ACTION_TYPE";
+const EMPLOYEE_CREATE_ACTION_TYPE = "EMPLOYEE_CREATE_ACTION_TYPE";
+const EMPLOYEE_UPDATE_ACTION_TYPE = "EMPLOYEE_UPDATE_ACTION_TYPE";
+const EMPLOYEE_DELETE_ACTION_TYPE = "EMPLOYEE_DELETE_ACTION_TYPE";
+
+// ACTIONS
+export const getAllEmployeeAction = () => {
+  return async (dispatch) => {
+    // API CALL
+    const url = `http://localhost:8080/api/employee/`;
+    const response = await axios.get(url);
+
+    console.log(response);
+
+    // UI UPDATE
+    dispatch({ type: "EMPLOYEE_GET_ALL_ACTION_TYPE", payload: response.data });
+  };
+};
+
+// REDURE FOR STATE UPDTE
 function EmployeeReducer(state = initState, action) {
-  return state;
+  switch (action.type) {
+    case EMPLOYEE_GET_ALL_ACTION_TYPE:
+      return { ...state, employeeList: action.payload };
+    default:
+      return state;
+  }
 }
 
 // MIDDLEWARE FOR THE ASYNC OPOERATION
